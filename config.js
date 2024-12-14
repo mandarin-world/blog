fb00d8329511cdb41ab71133b4d869fa264d0923
mandarin-world <momolovesmeme@naver.com>
2024-12-15 01:32:13 +0900

    변경 오류

--- config.js
+++ config.js
@@ -14,8 +14,8 @@ const users = [
   {
     id: 0, // default author
     username: "mandarin",
-    company: "",
-    position: "",
+    company: "weniv",
+    position: "CEO",
     img: "img/user/mandarin.jpg",
   },
 ];
